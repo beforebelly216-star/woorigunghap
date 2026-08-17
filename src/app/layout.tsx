f2c1f2ef549c0d4c9576928313478daa1b6ce858
beforebelly216-star <beforebelly216@gmail.com>
2026-08-17 17:34:50 +0900

    feat: connect legal footer sitewide

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { SiteFooter } from "@/components/site-footer";
 import { SiteHeader } from "@/components/site-header";
 import "./globals.css";
 import "./report-extra.css";
@@ -28,7 +29,8 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
     >
       <body className="min-h-full flex flex-col">
         <SiteHeader />
-        {children}
+        <div className="site-content">{children}</div>
+        <SiteFooter />
       </body>
     </html>
   );
