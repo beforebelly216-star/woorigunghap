37630683e52b28fb3a1a1cd63882a53c9c5a56a4
beforebelly216-star <beforebelly216@gmail.com>
2026-08-21 21:47:01 +0900

    brand: rename service to 우리사주

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -3,7 +3,7 @@ import { AuthStatus } from "@/components/auth-status";
 
 export function SiteHeader() {
   return <header className="site-header">
-    <Link href="/" className="site-brand">우리궁합</Link>
+    <Link href="/" className="site-brand">우리사주</Link>
     <AuthStatus />
   </header>;
 }
