bd409eaa1e12670fa49e7a5c8ba0412f64987a79
beforebelly216-star <beforebelly216@gmail.com>
2026-08-21 21:47:12 +0900

    brand: update metadata to 우리사주

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,8 +21,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "우리궁합 | 관계 궁합 리포트",
-  description: "생년월일시와 관계 유형을 바탕으로 연인, 친구, 직장동료의 관계 궁합을 정리해 드리는 우리궁합 리포트입니다.",
+  title: "우리사주 | 관계 궁합 리포트",
+  description: "생년월일시와 관계 유형을 바탕으로 연인, 친구, 직장동료의 관계 궁합을 정리해 드리는 우리사주 리포트입니다.",
 };
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
